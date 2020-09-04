6f2901e3a8aa79229d543841873a95f2530d3d69
s3539822 <s3539822@student.rmit.edu.au>
2020-09-04 17:09:44 +1000

    Comit

---
+++ server/checkout.route.ts
@@ -0,0 +1,64 @@
+/* tslint:disable:no-trailing-whitespace */
+import {Request, Response} from 'express';
+
+
+
+interface RequestInfo {
+  courseId: string;
+  callbackUrl: string;
+}
+
+export async function createCheckoutSession(req: Request, res: Response) {
+  try {
+    const info: RequestInfo = {
+      courseId: req.body.courseId,
+      callbackUrl: req.body.callbackUrl
+    };
+
+    let sessionConfig;
+
+    if (info.courseId) {
+      sessionConfig = setupPurchaseCourseSession(info);
+    }
+
+    console.log(sessionConfig);
+
+    //const session = await stripe.checkout.sessions.create(sessionConfig);
+
+    // /console.log(session);
+
+
+
+    res.status(200).send();
+  } catch (e) {
+    console.log('Error', e);
+    res.status(500).json({error: 'Error'});
+  }
+}
+
+function setupPurchaseCourseSession(info: RequestInfo) {
+  const config = setupBaseSessionConfig(info);
+
+  config.line_items = [{
+    name: 'sdf',
+    description: 'fds',
+    amount: 1500,
+    currency: 'aud',
+    quantity: 2,
+  }];
+
+
+
+
+  return config;
+}
+
+function setupBaseSessionConfig(info: RequestInfo) {
+  const config: any = {
+    payment_method_types: ['card'],
+    success_url: `${info.callbackUrl}/?purchaseResult=success`,
+    cancel_url: `${info.callbackUrl}/?purchaseResult=failed`
+  };
+
+  return config;
+}
